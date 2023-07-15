7df9467c3ab8ceaa5273e46545e5974c929fbf0a
sideeq12 <abdwaheed2018@gmail.com>
2023-07-15 19:02:39 +0100

    nested route

--- app/overview/page.jsx
+++ app/overview/page.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import Maindrop from "../../components/Maindrop"
+import Image from 'next/image'
+import full from "./web.png"
 
 const Overview = () => {
   return (
     <div className='flex'>
         <Maindrop />
-        <div className=''>
-            Main info
+        <div className='md:pt-32 pl-10'>
+            <h2 className='text-3xl font-bold'>
+               Course Overview
+            </h2>
+            <div className=' md:w-[650px] mt-10'>
+            Web development has become an essential skill in today's digital world, and the demand for proficient web developers is constantly on the rise. Whether you're a beginner looking to learn the ropes or an experienced developer seeking to level up your skills, Web Development Mastery is the ultimate teaching platform designed to empower
+             and guide you on your journey to becoming a web development expert.      </div>
+             <div className=' md:w-[650px] mt-10 mb-10'>
+             We offer a comprehensive curriculum that covers
+              all essential aspects of web development, catering to learners of all 
+              levels. From fundamental concepts like <span className='font-bold'> HTML, CSS,</span> and 
+              <span className='font-bold'>JavaScript</span> to advanced topics such as front-end and back-end frameworks, databases, and server management, the platform provides a structured
+              learning path to ensure you acquire a solid foundation and advanced expertise
+             </div>
+             <p className='mb-10'>Get enough skills to create a responsive design below</p>
+             <div>
+                <Image src={full} alt='image layout'  width={400} height={400}/> 
+             </div>
+
+             <div className='mt-10'>
+                <h3>Key features</h3>
+                <p className='md:w-[650px] mt-10 mb-10'>
+                Interactive Learning: Engaging and interactive learning
+                 experiences are at the core of the platform. Through a combination of video tutorials, quizzes, coding exercises, and real-world projects, learners can apply their knowledge in a practical setting. The platform provides a sandbox environment for learners to experiment with code and 
+                see the immediate results of their HTML and CSS implementations.
+                </p>
+                <p className='md:w-[650px] mt-10 mb-10'>
+                Personalized Learning Paths: The platform recognizes that each learner has unique goals and learning preferences. It offers personalized learning paths that adapt to the individual's progress, allowing them to learn at their own pace. Learners can also set goals, track their progress, 
+                and receive tailored recommendations to optimize their learning journey.
+                </p>
+                <p className='md:w-[650px] mt-10 mb-10'>
+                Industry-Relevant Projects: To reinforce the learned concepts, the platform offers hands-on projects inspired by real-world scenarios. These projects provide learners with practical experience in building responsive websites, optimizing performance, and implementing best practices.
+                 By working on these projects, learners can build a 
+                </p>
+                <p className='md:w-[650px] mt-10 mb-10'>
+                Mobile-Friendly Learning: Recognizing the importance of mobile devices in today's digital landscape, the platform is optimized for mobile learning. 
+                </p>
+
+             </div>
         </div>
     </div>
   )
--- components/Maindrop.jsx
+++ components/Maindrop.jsx
@@ -1,18 +1,46 @@
 "use client";
 import Link from 'next/link';
 import React from 'react'
+import { useState } from 'react';
 
 
 const Maindrop = () => {
-  return (
-        <div className='w-fit z-10 md:w-2/6 bg-white flex flex-col gap-8 pl-10 pt-16 pb-10'>
+    const [isActive, setActive ] = useState(false);
+    const [open, setOpen] = useState(true)
+
+    const changeView = () => {
+        setActive(!isActive)
+    }
+
+  return (<>
+        {open ?  <div className='w-fit z-10 md:w-3/12 bg-white flex flex-col gap-8 pl-10 pt-16 pb-10'>
+            <span onClick={(open)=>setOpen(!open)} className='cursor-pointer ml-auto mr-16'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+            </span>
             <div className='mt-20 ml-10'>Logo</div>
             <Link className='hover:border-l-2  md:py-3 hover:border-black pl-10 cursor-pointer'
              href="/">Overview</Link>
 
-            <Link 
+         <div className=''>
+         <button onClick={changeView}
             className='hover:border-l-2 md:py-3 hover:border-black pl-10 cursor-pointer' 
-            href="/">Introduction to HTML</Link>
+            >Introduction to HTML</button>
+            { isActive ? <div className='border-2 pt-3 border-gray-500 pl-10 mt-5 flex flex-col gap-4'>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            <Link href="/">Html tags</Link>
+            </div> : null}
+         </div>
 
             <Link 
             className='hover:border-l-2 md:py-3 hover:border-black pl-10 cursor-pointer'
@@ -30,7 +58,12 @@ const Maindrop = () => {
 
             <Link className='hover:border-l-2 md:py-3 hover:border-black pl-10 cursor-pointer'
              href="/">Ask a question</Link>
-        </div>
+        </div>: <span className='px-10 pt-10 mr-10 bg-white' >
+        <svg onClick={(open)=>setOpen(!open)} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
+            </span>}
+  </>
   )
 }
 

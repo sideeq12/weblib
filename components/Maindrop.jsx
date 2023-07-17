7e782243e234110a66d55991d1ee72957b63128a
sideeq12 <abdwaheed2018@gmail.com>
2023-07-17 11:31:51 +0100

    nav bar

--- components/Maindrop.jsx
+++ components/Maindrop.jsx
@@ -13,7 +13,7 @@ const Maindrop = () => {
     }
 
   return (<>
-        {open ?  <div className='w-fit z-10 md:w-3/12 bg-white flex flex-col gap-8 pl-10 pt-16 pb-10'>
+        {open ?  <div className='w-fit z-10 md:w-3/12 bg-white flex flex-col gap-2 pl-10 pt-16 pb-10'>
             <span onClick={(open)=>setOpen(!open)} className='cursor-pointer ml-auto mr-16'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>

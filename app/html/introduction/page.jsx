b9c4848a35269ce18de468408e7b347e9b797c4a
sideeq12 <abdwaheed2018@gmail.com>
2023-07-19 17:33:34 +0100

    settiing home section

--- app/html/introduction/page.jsx
+++ app/html/introduction/page.jsx
@@ -3,7 +3,7 @@ import Maindrop from '@/components/Maindrop'
 import Image from 'next/image'
 import Banner from '@/components/banner'
 import Code from '@/components/code'
-// import full from "./web.png"
+import full from "./helloWorld.png"
 
 const Overview = () => {
   return (
@@ -13,28 +13,39 @@ const Overview = () => {
             <h2 className='text-3xl font-bold'>
                Hello World!
             </h2>
+
             <Banner info={ `In this section, you are going to write your first line of code
                and display it on your browser.
                 Our purpose in this course is to make the content very 
                friendly and familiar inorder to ease your learning progress by connecting all 
                the fundamental concepts.`  } />
+
             <Banner info={`HTML (HyperText Markup Language) is the standard markup language used to create web pages. It provides the structure and content of a webpage and uses tags to define elements within the page. The "Hello, World!" section is a simple example used to introduce beginners to HTML coding. It's a tradition in programming to start with this basic example to
              get familiar with the syntax and how things work. Let's dive in!`} />
             <Code>
-               <span className='text-orange-300'>{"<h1>"}</span> Hello World
-               <span className='text-orange-300'>{" </h1>"}</span> 
+               <span className='text-orange-300'>{"<html>"} <br/></span> 
+               <span className='text-orange-300'>
+               <label className='text-stone-800 ' htmlFor=""> .....</label> {"<head>"} <br/></span> 
+               <span className='text-orange-300'>
+                  <label className='text-stone-800 ' htmlFor=""> ........</label> {"<body>"} <br/></span> 
+                  <label className='text-stone-800 ' htmlFor=""> .----.......</label>  {"Hello, World!"} <br />
+
+               <span className='text-orange-300'>
+                  <label className='text-stone-800 ' htmlFor=""> ........</label> {"</body>"} <br/></span> 
+               <span className='text-orange-300'>
+               <label className='text-stone-800 ' htmlFor=""> ......</label> {"</head>"} <br/></span> 
+               <span className='text-orange-300'>{"</html>"} <br/></span> 
             </Code>
-             <div className=' md:w-[650px] mt-10 mb-10'>
-             We offer a comprehensive curriculum that covers
-              all essential aspects of web development, catering to learners of all 
-              levels. From fundamental concepts like <span className='font-bold'> HTML, CSS,</span> and 
-              <span className='font-bold'>JavaScript</span> to advanced topics such as front-end and back-end frameworks, databases, and server management, the platform provides a structured
-              learning path to ensure you acquire a solid foundation and advanced expertise
+            <p className='my-3 md:my-6'>Out put on the browser</p>
+            <div className=' my-3 md:my-5'>
+                <Image className='border border-black' src={full} alt='image layout'  width={400} height={400}/> 
              </div>
-             <p className='mb-10'>Get enough skills to create a responsive design below</p>
-             <div>
-                {/* <Image src={full} alt='image layout'  width={400} height={400}/>  */}
-             </div>
+            <p className='my-3 md:my-6'>Code explanation</p>
+             <ul>
+               <li> DOCTYPE html: This is the document type declaration and specifies that the document is an HTML5 document.
+                It helps the browser to understand the version of HTML being used</li>
+             </ul>
+             
 
              <div className='mt-10'>
                 <h3>Key features</h3>

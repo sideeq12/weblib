38244c819d1a432ac3e6955294848bbd3f1fc926
sideeq12 <abdwaheed2018@gmail.com>
2023-07-25 22:48:53 +0100

    complete link section

--- app/html/Links/page.jsx
+++ app/html/Links/page.jsx
@@ -3,62 +3,65 @@ import Maindrop from '../../../components/Maindrop'
 import Image from 'next/image'
 import Banner from "../../../components/banner"
 import Code from "../../../components/code"
-// import full from "./web.png"
+import mylink from "./link.png"
+import mylres from "./linkres.png"
+import extern from './external.png'
+import exres from './extres.png'
+import mail from './mail.png'
+import mailre from './mailres.png'
+import Link from 'next/link'
 
 const Overview = () => {
   return (
     <div className='flex bg-white'>
         <Maindrop />
-        <div className='md:pt-32 md:pl-10 pl-5 pt-10 pr-5'>
+        <div className='md:pt-32 md:pl-10 pl-5 pt-10 pb-10 pr-5'>
 
             <h2 className='text-3xl font-bold'>
                Links in HTML
             </h2>
-
-            <Banner info={ `In this section, you are going to write your first line of code
-               and display it on your browser.
-                Our purpose in this course is to make the content very 
-               friendly and familiar inorder to ease your learning progress by connecting all 
-               the fundamental concepts.`  } />
-            <Banner info={`HTML (HyperText Markup Language) is the standard markup language used to create web pages. It provides the structure and content of a webpage and uses tags to define elements within the page. The "Hello, World!" section is a simple example used to introduce beginners to HTML coding. It's a tradition in programming to start with this basic example to
-             get familiar with the syntax and how things work. Let's dive in!`} />
             <Code>
-               <span className='text-orange-300'>{"<h1>"}</span> Hello World
-               <span className='text-orange-300'>{" </h1>"}</span> 
+               <span className='text-orange-300'>{"<a href='url'>"}</span> website name
+               <span className='text-orange-300'>{" </a>"}</span> 
             </Code>
 
-             <div className=' md:w-[650px] mt-10 mb-10'>
-             We offer a comprehensive curriculum that covers
-              all essential aspects of web development, catering to learners of all 
-              levels. From fundamental concepts like <span className='font-bold'> HTML, CSS,</span> and 
-              <span className='font-bold'>JavaScript</span> to advanced topics such as front-end and back-end frameworks, databases, and server management, the platform provides a structured
-              learning path to ensure you acquire a solid foundation and advanced expertise
+             <div className=' md:w-[650px] mt-10 mb-10'>In HTML, you can
+              create hyperlinks (links) using the anchor tag {" <a>"}. Links
+                allow users to navigate from one web page to another or to different sections within the same page. The anchor tag has an attribute called href, which
+                specifies the destination URL or target of the link.
              </div>
-             <p className='mb-10'>Get enough skills to create a responsive design below</p>
              <div>
-                {/* <Image src={full} alt='image layout'  width={400} height={400}/>  */}
+                <Image src={mylink} alt='image layout'  width={400} height={400}/> 
+             </div>
+             <div className='my-10'>
+                <Image className='border' src={mylres} alt='image layout'  width={400} height={400}/> 
              </div>
 
              <div className='mt-10'>
                 <h3>Key features</h3>
                 <p className='md:w-[650px] mt-10 mb-10'>
-                Interactive Learning: Engaging and interactive learning
-                 experiences are at the core of the platform. Through a combination of video tutorials, quizzes, coding exercises, and real-world projects, learners can apply their knowledge in a practical setting. The platform provides a sandbox environment for learners to experiment with code and 
-                see the immediate results of their HTML and CSS implementations.
+                  <div><span className='font-bold'>URL</span> :  is the target URL, which can be an external website address (e.g., https://www.example.com) or a relative
+                      path to another page within the same website (e.g.,about.html). </div>
+                      <div> <span className="font-bold">Link Test</span> is the text that will be displayed as the clickable link.</div>
                 </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Personalized Learning Paths: The platform recognizes that each learner has unique goals and learning preferences. It offers personalized learning paths that adapt to the individual's progress, allowing them to learn at their own pace. Learners can also set goals, track their progress, 
-                and receive tailored recommendations to optimize their learning journey.
-                </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Industry-Relevant Projects: To reinforce the learned concepts, the platform offers hands-on projects inspired by real-world scenarios. These projects provide learners with practical experience in building responsive websites, optimizing performance, and implementing best practices.
-                 By working on these projects, learners can build a 
-                </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Mobile-Friendly Learning: Recognizing the importance of mobile devices in today's digital landscape, the platform is optimized for mobile learning. 
-                </p>
-
+                <div>
+                <Image src={extern} alt='image layout'  width={400} height={400}/> 
              </div>
+             <div className='my-10'>
+                <Image src={exres} className='border' alt='image layout'  width={400} height={400}/> 
+             </div>
+                
+               <span className="font-bold my-10">Link to an email address (using the mailto protocol):</span>
+               <div>
+                <Image src={mail} alt='image layout'  width={400} height={400}/> 
+             </div>
+             <div className='my-10'>
+                <Image src={mailre} className='border' alt='image layout'  width={400} height={400}/> 
+             </div>
+                
+             </div>
+
+             <Link className='px-10 py-3 my-10 border ' href='/html/' >Next</Link>
         </div>
     </div>
   )

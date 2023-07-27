5137738400a380ec499a3fec1e36cc076aac70c0
sideeq12 <abdwaheed2018@gmail.com>
2023-07-27 13:21:06 +0100

    form

--- app/html/forms/page.jsx
+++ app/html/forms/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Maindrop from "../../../components/Maindrop"
 import Image from 'next/image'
-// import full from "./web.png"
+import full from "./form.png"
+import fulle from "./res.png"
 
 const Overview = () => {
   return (
@@ -18,11 +19,11 @@ const Overview = () => {
               a form and clicks the "Submit" button, 
             the data is sent to a server for processing or storage. </div>
 
-             <p className='my-5'>To create a form in HTML, you use the
+             <p className='my-5 md:w-[650px]'>To create a form in HTML, you use the
               <span className="form-bold">form </span> element along with various form-related 
                elements and attributes. The most commonly used form elements are:</p>
-               <div className='my-5 flex flex-col gap-3'>
-                  <div>
+               <div className='my-5 flex flex-col gap-3 md:w-[650px]'>
+                  <div className='md:w-[650px]'>
                      <span className="font-bold">form : </span> This is the main 
                      container element for the form. It encompasses all the form elements and defines how
                       the data will be sent to the server.
@@ -50,30 +51,14 @@ const Overview = () => {
                   </div>
 
                </div>
+               <span className="my-10">Below is an example of form for submiting data.</span>
              <div>
-                {/* <Image src={full} alt='image layout'  width={400} height={400}/>  */}
+                <Image src={full} alt='image layout'  width={400} height={400}/> 
+             </div>
+             <div>
+                <Image className='border my-5' src={fulle} alt='image layout'  width={400} height={400}/> 
              </div>
 
-             <div className='mt-10'>
-                <h3>Key features</h3>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Interactive Learning: Engaging and interactive learning
-                 experiences are at the core of the platform. Through a combination of video tutorials, quizzes, coding exercises, and real-world projects, learners can apply their knowledge in a practical setting. The platform provides a sandbox environment for learners to experiment with code and 
-                see the immediate results of their HTML and CSS implementations.
-                </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Personalized Learning Paths: The platform recognizes that each learner has unique goals and learning preferences. It offers personalized learning paths that adapt to the individual's progress, allowing them to learn at their own pace. Learners can also set goals, track their progress, 
-                and receive tailored recommendations to optimize their learning journey.
-                </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Industry-Relevant Projects: To reinforce the learned concepts, the platform offers hands-on projects inspired by real-world scenarios. These projects provide learners with practical experience in building responsive websites, optimizing performance, and implementing best practices.
-                 By working on these projects, learners can build a 
-                </p>
-                <p className='md:w-[650px] mt-10 mb-10'>
-                Mobile-Friendly Learning: Recognizing the importance of mobile devices in today's digital landscape, the platform is optimized for mobile learning. 
-                </p>
-
-             </div>
         </div>
     </div>
   )

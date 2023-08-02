ca04473e25a205578178bccae52d6c00653c8c77
sideeq12 <abdwaheed2018@gmail.com>
2023-08-02 22:46:33 +0100

    responsiveness

--- app/css/responsiveness/page.jsx
+++ app/css/responsiveness/page.jsx
@@ -10,11 +10,10 @@ const Page = () => {
                Responsiveness
             </h2>
             <div className=' md:w-[650px] mt-10 text-sm md:text-base '>
-            In HTML, the <span className="font-bold">audio </span> and 
-             <span className="font-bold">video </span>
-               tags are used to embed audio and video content, respectively, into a web page.
-                These elements allow you to play multimedia files directly within the browser without 
-               requiring any third-party plugins.   </div>
+            Responsiveness in CSS refers to the ability of a web page or application to adapt and adjust 
+            its layout and design to different screen sizes, devices, and orientations. With the vast variety
+             of devices used to access the internet, from large desktop monitors to small smartphones, creating
+             responsive designs is crucial to provide a consistent and user-friendly experience for all users.   </div>
              <div className=' md:w-[650px] my-10'>
              The <span className="font-bold">audio </span> tag is used to embed audio content, such as music, sound effects, 
                or podcasts, into a web page. It supports various audio file formats, including 

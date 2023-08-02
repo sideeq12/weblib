f0e0614f5d312a8a1603ea9cd04f390576098f67
sideeq12 <abdwaheed2018@gmail.com>
2023-08-02 13:55:26 +0100

    color details

--- app/css/css-colors/page.jsx
+++ app/css/css-colors/page.jsx
@@ -7,23 +7,45 @@ const Page = () => {
         <Maindrop />
         <div className='md:pt-32 md:pl-10 pl-5 pt-10 pr-5'>
             <h2 className='text-3xl font-bold'>
-               Audio and video tag in HTML 
+              Css colors
             </h2>
             <div className=' md:w-[650px] mt-10 text-sm md:text-base '>
-            In HTML, the <span className="font-bold">audio </span> and 
-             <span className="font-bold">video </span>
-               tags are used to embed audio and video content, respectively, into a web page.
-                These elements allow you to play multimedia files directly within the browser without 
-               requiring any third-party plugins.   </div>
-             <div className=' md:w-[650px] my-10'>
-             The <span className="font-bold">audio </span> tag is used to embed audio content, such as music, sound effects, 
-               or podcasts, into a web page. It supports various audio file formats, including 
-               MP3, WAV, and Ogg.
-                Here's an example of how to use the <span className="font-bold">audio </span> tag:
-             </div>
-             <div>
-                {/* <Image src={full} alt='image layout'  width={400} height={400}/>  */}
-             </div>
+            In CSS (Cascading Style Sheets), colors are used to define the appearance of various
+             elements on a web page. CSS allows you to specify colors in different formats 
+            to achieve the desired visual effect. Here are the common ways to represent 
+            colors in CSS:   </div>
+            <div className="my-5">
+                  <ul>
+                     <li> <span className="font-bold">Named colors : </span> 
+                     CSS provides a set of predefined color names that you can use directly without
+                      specifying their hex or RGB values. 
+                     Some examples of named colors are red, blue, green, yellow, black, white, etc.</li>
+                     <li> <span className="font-bold">Hexadecimal Notation : </span> 
+                     Colors can be represented using a six-digit hexadecimal value. Each pair of digits represents 
+                     the red, green, and blue (RGB) components of the color. For example, 
+                     #FF0000 represents pure red, #00FF00 represents pure green, and #0000FF represents pure blue.
+                     and have a cursive appearance..</li>
+                     <li> <span className="font-bold">RGB Notation : </span> 
+                     Colors can also be represented using the rgb() function, specifying the red, green,
+                      and blue components individually. Each component ranges from 0 to 255. For example, 
+                      rgb(255, 0, 0) represents pure red, rgb(0, 255, 0)
+                      represents pure green, and rgb(0, 0, 255) represents pure blue..</li>
+                     <li> <span className="font-bold">RGBA Notation : </span> 
+                     Similar to RGB, you can use the rgba() function to represent colors with an additional
+                      alpha (transparency) component. The alpha value ranges from 0 (fully transparent) to 1
+                       (fully opaque). 
+                     For example, rgba(255, 0, 0, 0.5) represents semi-transparent red..</li>
+                     <li> <span className="font-bold">HSL Notation : </span> 
+                     HSL stands for Hue, Saturation, and Lightness. It provides a more intuitive way to
+                      describe colors. The hsl() function allows you to specify the hue (0 to 360), saturation
+                       (0% to 100%), and lightness
+                      (0% to 100%) components. For example, hsl(0, 100%, 50%) represents pure red.</li>
+                      <li> <span className="font-bold">HSLA Notation : </span> 
+                      Similar to HSL, you can use the hsla() function to represent colors with an additional alpha (transparency) component. The alpha value ranges from 0 (fully transparent)
+                       to 1 (fully opaque). For example, hsla(0, 100%, 50%, 0.5) represents semi-transparent red.</li>
+                    
+                  </ul>
+                </div>
 
              <div className='mt-10'>
                 <h3>Result on the browser</h3>

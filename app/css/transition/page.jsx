1b26a6f1676f2d6f8be6fd01f9b3d8f455a55463
sideeq12 <abdwaheed2018@gmail.com>
2023-08-03 13:43:46 +0100

    gif

--- app/css/transition/page.jsx
+++ app/css/transition/page.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Maindrop from '@/components/Maindrop'
+import Image from 'next/image'
+import defa from './transdefault.png'
+import fad  from './gaf.gif'
+import cod from './codeg.png'
 
 const Page = () => {
   return (
@@ -18,52 +22,33 @@ const Page = () => {
                for a particular CSS property, and the browser will automatically generate the animation 
                between those two states. This creates a more engaging and visually appealing user experience.
              </div>
-             <div>
-                {/* <Image src={full} alt='image layout'  width={400} height={400}/>  */}
-             </div>
+            <h3 className="font-semibold">Basic illustration</h3>
+            <div>
+         <Image src={defa} width={400} height={400} />
+            </div>
 
-             <div className='mt-10'>
-                <h3>Result on the browser</h3>
+             <div className="my-5 md:w-[650px]">
+                  <ul className='flex flex-col gap-3'>
+                     <li> <span className="font-bold">Selectors : </span>
+                     The CSS selector that targets the element you want to apply the transition to.</li>
+                     <li> <span className="font-bold">Property : </span>The CSS property you want to animate during the transition.</li>
+                     <li> <span className="font-bold">duration : </span> 
+                     The time it takes for the transition to complete,
+                      usually specified in seconds (s) or milliseconds (ms).</li>
+                     <li> <span className="font-bold">timing-in-function : </span> 
+                     Optional. Specifies the acceleration curve for the transition. It controls the speed of
+                      the animation at 
+                     various points. Common values are linear, ease, ease-in, ease-out, and ease-in-out, among others.</li>
+                     <li> <span className="font-bold">delay : </span> Optional. The time to wait before starting the transition 
+                     after the property change is triggered. Also specified in seconds (s) or milliseconds (ms)..</li>
+                  </ul>
+                </div>
+                <h3 className="font-semibold mb-5">Example</h3>
                 <div>
-                {/* <Image className='border' src={res} alt='image layout'  width={400} height={400}/>  */}
-             </div>
-                <p className='md:w-[650px] my-10'>
-                In this example, the controls attribute adds basic playback controls 
-                (play, pause, volume, etc.) to the audio player. The <span className="font-bold">source </span>
-                element is used to specify multiple audio file formats to provide
-                 compatibility across different browsers. The browser will automatically
-                 choose the supported format based on the browser's capabilities.
-                </p>
-
-                <h3 className='my-5'>The video tag </h3>
-                <p className='md:w-[650px] my-10'>
-                The <span className="font-bold">video </span> tag is used to embed video content, such as movies, video clips, 
-                  or video presentations, into a web page. It supports various video file formats, including MP4,
-                   WebM, and Ogg. Here's an example of how to use the <span className="font-bold">video </span> tag: 
-                </p>
-                <div>
-                {/* <Image src={fim} alt='image layout'  width={400} height={400}/>  */}
-             </div>
-             <h3>Result on the browser</h3>
-                <div>
-                {/* <Image className='border' src={vres} alt='image layout'  width={400} height={400}/>  */}
-             </div>
-             <p className="my-10 w-[650px]">
-             Similar to the <span className="font-bold">audio </span> tag, the controls attribute adds playback controls to the video player. The width and height attributes define the dimensions of the video player.
-
-Both the <span className="font-bold">audio </span> and <span className="font-bold">video </span> tags can have multiple <span className="font-bold">source </span> elements, allowing you to provide different file formats 
-to improve cross-browser compatibility.
- <br /> The browser will choose the first supported format from the list.
-
-If the browser does not support the <span className="font-bold">audio </span> or <span className="font-bold">video </span>tags or any of the specified formats, it will display the content inside the tags' fallback content. In the examples above, the fallback content is the message "Your browser does not support the audio/video element." This allows you to provide alternative content or instructions for users 
-   whose browsers don't support the audio or video features.
-             </p>
-             <h3>When the correct video name and extension available: </h3>
-                <div>
-                {/* <Image className='border' src={correct} alt='image layout'  width={400} height={400}/>  */}
-             </div>
-
-             </div>
+         <Image src={cod} width={400} height={400} />
+            </div>  <div>
+         <Image src={fad} width={400} height={400} className='border my-5' />
+            </div>
         </div>
     </div>
   )

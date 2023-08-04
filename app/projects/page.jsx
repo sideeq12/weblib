f9dbcc0ac27fa72afe3072f133a2cc586794e9a5
sideeq12 <abdwaheed2018@gmail.com>
2023-08-04 05:40:07 +0100

    complete practices

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -5,12 +5,13 @@ import Maindrop from '@/components/Maindrop'
 import logi from './login.png'
 import  simp from './simple.png'
 import teach from './teacher.png'
+import test from './test.png'
 
 function Projects() {
     let loginUrl = 'https://www.behance.net/gallery/173924583/Fitness-Web-App-Login-and-Register/modules/981976623'
     let simplelog = 'https://www.behance.net/gallery/171258351/Login-and-Sgin-up-Page/modules/966430495'
     let teacherland = 'https://www.behance.net/gallery/176236823/English-Teachers-Landing-Page/modules/995347235'
-  
+  let testmo = 'https://www.behance.net/gallery/128945099/TESTIMONIAL-PAGE/modules/757290519'
   
   return (<div className='flex bg-white'>
   <Maindrop />
@@ -24,32 +25,32 @@ function Projects() {
          kindly go through the following simple design and implement them with the skills learned in 
          the previous chapters
 
-         <div className="my-5">
+         <div className="my-10">
+            <h2 className='mb-4'>Testimonial Card Design</h2>
+            <Image src={test} width={400} height={400} />
+            <Link className='font-semibold my-5 underline text-blue-500' href={testmo}>Check design</Link>
+             </div>
+         <div className="my-10">
             <h2 className='mb-4'>Fitness Login</h2>
             <Image src={logi} width={400} height={400} />
             <Link className='font-semibold my-5 underline text-blue-500' href={loginUrl}>Check design</Link>
              </div>
 
-             <div className="my-5">
+             <div className="my-10">
             <h2 className='mb-4'>Neat Login</h2>
             <Image src={simp} width={400} height={400} />
             <Link className='font-semibold my-5 underline text-blue-500' href={simplelog}>Check design</Link>
              </div>
 
-             <div className="my-5">
+             <div className="my-10">
             <h2 className='mb-4'>Teaching landing page</h2>
             <Image src={teach} width={400} height={400} />
             <Link className='font-semibold my-5 underline text-blue-500' href={teacherland}>Check design</Link>
              </div>
 
-             <div className="my-5">
-            <h2 className='mb-4'>Fitness Login</h2>
-            <Image src={logi} width={400} height={400} />
-            <Link className='font-semibold my-5 underline text-blue-500' href={simplelog}>Check design</Link>
-             </div>
           </div>
 
-       <button className='border py-3 px-8'>Next</button>
+       <Link className='border py-3 px-8' href='https://www.behance.net'>more</Link>
 
   </div>
 </div>

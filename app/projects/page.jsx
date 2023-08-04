178cf213fd57f241492fc46f3e2d5cd0dc7d947c
sideeq12 <abdwaheed2018@gmail.com>
2023-08-04 05:30:50 +0100

    design

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Maindrop from '@/components/Maindrop'
+import logi from './login.png'
+import  simp from './simple.png'
+import teach from './teacher.png'
 
 function Projects() {
     let loginUrl = 'https://www.behance.net/gallery/173924583/Fitness-Web-App-Login-and-Register/modules/981976623'
     let simplelog = 'https://www.behance.net/gallery/171258351/Login-and-Sgin-up-Page/modules/966430495'
+    let teacherland = 'https://www.behance.net/gallery/176236823/English-Teachers-Landing-Page/modules/995347235'
+  
+  
   return (<div className='flex bg-white'>
   <Maindrop />
   <div className='md:pt-32 md:pl-10 pl-5 pt-10 pr-5 pb-10'>
@@ -19,9 +25,28 @@ function Projects() {
          the previous chapters
 
          <div className="my-5">
-            <h2>Fitness Login</h2>
-            <Image src={loginUrl} width={400} height={400} />
-         </div>
+            <h2 className='mb-4'>Fitness Login</h2>
+            <Image src={logi} width={400} height={400} />
+            <Link className='font-semibold my-5 underline text-blue-500' href={loginUrl}>Check design</Link>
+             </div>
+
+             <div className="my-5">
+            <h2 className='mb-4'>Neat Login</h2>
+            <Image src={simp} width={400} height={400} />
+            <Link className='font-semibold my-5 underline text-blue-500' href={simplelog}>Check design</Link>
+             </div>
+
+             <div className="my-5">
+            <h2 className='mb-4'>Teaching landing page</h2>
+            <Image src={teach} width={400} height={400} />
+            <Link className='font-semibold my-5 underline text-blue-500' href={teacherland}>Check design</Link>
+             </div>
+
+             <div className="my-5">
+            <h2 className='mb-4'>Fitness Login</h2>
+            <Image src={logi} width={400} height={400} />
+            <Link className='font-semibold my-5 underline text-blue-500' href={simplelog}>Check design</Link>
+             </div>
           </div>
 
        <button className='border py-3 px-8'>Next</button>

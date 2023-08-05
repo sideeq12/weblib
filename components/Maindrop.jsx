44e8c5442de671271be5e1430bcc81562f554cdd
sideeq12 <abdwaheed2018@gmail.com>
2023-08-05 16:14:50 +0100

    menu layer

--- components/Maindrop.jsx
+++ components/Maindrop.jsx
@@ -25,8 +25,8 @@ const Maindrop = () => {
         {open ? 
          <div className='w-9/12 bg-blue-900 text-sm pr-3 md:pr-10 z-10 pos  md:w-3/12 
          flex flex-col gap-2 pl-3 md:pl-6 pt-16 pb-5'>
-            <span onClick={(open)=>setOpen(!open)} className='cursor-pointer ml-auto mr-16'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+            <span onClick={(open)=>setOpen(!open)} className='cursor-pointer ml-auto '>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>
@@ -114,9 +114,9 @@ const Maindrop = () => {
             
              href="/questions">Ask a question</Link>
         </div> :
-         <div className='pt-10 px-5 md:px-10 md:pt-16  bg-slate-100 ' onClick={setToOpen}>
+         <div className='pt-10 px-5 md:px-10 md:pt-16  bg-blue-900 ' onClick={setToOpen}>
           <span className='cursor-pointer'  >
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-list" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
 </svg>
             </span>
